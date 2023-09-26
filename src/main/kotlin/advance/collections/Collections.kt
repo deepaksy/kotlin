4f706a4e6a5363268385f04a1cc939314d634d38
deepaksy <deepaksy28@gmail.com>
2023-09-26 11:08:01 +0530

    initial commit

--- src/main/kotlin/advance/collections/Collections.kt
+++ src/main/kotlin/advance/collections/Collections.kt
@@ -1,6 +1,8 @@
-package advance
+package advance.collections
 
+import java.util.ArrayList
 import java.util.LinkedList
+
 
 
 fun lists():List<String>{
@@ -14,10 +16,25 @@ fun lists():List<String>{
     return lists
 }
 
+fun arraylist():List<Int>{
+    val list:ArrayList<Int> = ArrayList<Int>()
+    list.add(1)
+    list.add(2)
+    list.add(3)
+    list.add(4)
+    list.add(5)
+    list.add(6)
+    return list
+}
+
 fun main(){
     val list:List<String> = lists()
     val item: Iterator<String> = list.iterator()
     for(i in item){
         println(i)
+    }
+    val iter: Iterator<Int> = arraylist().iterator()
+    while (iter.hasNext()){
+        println(iter.next())
     }
 }
